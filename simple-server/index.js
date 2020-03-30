f7835f4efa4d1dc4576574a64924b137e8f2db16
Matt Cavanagh <matt.c@darkynt.co.uk>
2020-03-30 16:31:34 +0100

    Quickie hookup of the basic routes

--- simple-server/index.js
+++ simple-server/index.js
@@ -1,11 +1,20 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 
+const Router = require('koa-router')
+const router = new Router()
+
+const userRouter = require('./routes/user')
+
+router.use('/user', userRouter.routes(), userRouter.allowedMethods())
+
 const {
   APP_PORT = 3000
 } = process.env
 
 const app = new Koa()
+
 app.use(bodyparser())
+app.use(router.routes(), router.allowedMethods())
 
 const server = app.listen(APP_PORT)
--- simple-server/routes/user.js
+++ simple-server/routes/user.js
@@ -1,10 +1,22 @@
-const Router = require('koa-router')
-const userRouter = new Router()
+const Router = require("koa-router");
+const setUser = require("../middleware/set-user");
+const { userToJwtString } = require("../services/fake-auth");
+const userRouter = new Router();
 
-router.get('/me', (ctx, next) => {
-  
-})
+router.get("/me", setUser, (ctx, next) => {
+  if (!ctx.user) ctx.throw(401, "Not Authorized");
+  if (ctx.user) {
+    ctx.status = 200;
+    ctx.body = ctx.user;
+  }
+});
 
-router.post('/me', ctx => {
-
-})
+router.post("/me", async ctx => {
+  const { user, email } = ctx.request.body;
+  ctx.cookies.set("bearer", userToJwtString({ user, email }), {
+    overwrite: true,
+    secure: true,
+    httpOnly: true,
+    maxAge: JWT_EXPIRY_MS
+  });
+});

b8859b81fdb54d2d82f1efb734c7157e5263bf15
Matt Cavanagh <matt.c@darkynt.co.uk>
2020-03-30 15:58:38 +0100

    Adds mock data

---
+++ simple-server/services/fake-course-db.js
@@ -0,0 +1,29 @@
+const courses = [
+  {
+    id: 0,
+    title: "Law",
+    description: "A Law Course ",
+    cost: 1000,
+    allocations: []
+  },
+  {
+    id: 1,
+    title: "Accounting",
+    description: "An Accounting Course ",
+    cost: 500,
+    classroom: true,
+    maxSeats: 10,
+    allocations: []
+  },
+  {
+    id: 2,
+    title: "Management",
+    description: "A Management Course ",
+    cost: 200,
+    classroom: true,
+    maxSeats: 10,
+    allocations: []
+  }
+];
+
+module.exports = {}

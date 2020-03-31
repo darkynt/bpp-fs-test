6c5e0e031052ba07ef2873e49af0cdee83914906
darkynt <matt.c@darkynt.co.uk>
2020-03-31 07:11:10 +0100

    Enable proxying; deal with 2020 tty bug

--- simple-server/index.js
+++ simple-server/index.js
@@ -1,11 +1,28 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
-
+const proxy = require('koa-proxies')
 const Router = require('koa-router')
 const router = new Router()
 
-const userRouter = require('./routes/user')
+const {
+  UI_HOST
+} = process.env
 
+/* Webpack dev proxy redirect - avoid CORS and api related coding */
+const liveProxy = proxy('/*', {
+  target: `http://${UI_HOST}`,
+  changeOrigin: false,
+  logs: true,
+})
+
+const websocketProxy = (req, socket, head) => {
+  proxy.proxy.ws(req, socket, head, {
+    target: `ws://${UI_HOST}`,
+  })
+}
+
+/* Assemble router middleware */
+const userRouter = require('./routes/user')
 router.use('/user', userRouter.routes(), userRouter.allowedMethods())
 
 const {
@@ -14,7 +31,10 @@ const {
 
 const app = new Koa()
 
+/* Enrol app middleware */
 app.use(bodyparser())
 app.use(router.routes(), router.allowedMethods())
+app.use(liveProxy)
 
 const server = app.listen(APP_PORT)
+server.on('upgrade', websocketProxy)
